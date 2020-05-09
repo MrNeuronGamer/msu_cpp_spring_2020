ba2909e477f6ce0e982031dbdcfd5b4e37736c0f
khoroshilov dmitry <khoroshilov.dmitry@mail.ru>
2020-05-10 00:00:46 +0300

    vector dev and fix

--- 07/test.cpp
+++ 07/test.cpp
@@ -1,283 +1,72 @@
-#pragma once
-#include <chrono>
+
 #include <iostream>
-
-#include <deque>
-#include <list>
+#include <memory>
+#include "vector.h"
 #include <vector>
+#include <string>
 
-#include "vector.cpp"
+using namespace std;
 
-template <class T>
-int benchmark(T& c)
+int main(int argc, char const *argv[])
 {
-    const int N = 1000000;
+    MyVector<int> v; 
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(123);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(123);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(123);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(123);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(123);
+   
+    cout << v[10] << endl;
+    MyVector<int> v2;
+    v2.operator=(v);
+    v2.pop_back();
+    cout << v2.capacity() << endl;
+    v2.shrink_to_fit();
+    cout << v2.capacity() << endl;
+    
+    v2.resize(1,-1);
+    cout << v2.capacity() << v2.size() << endl;
+  
 
-    int res = 0;
-
-    for (int j = 0; j < 20; ++j)
+    MyVector<string> v3;
+    v3.push_back("qwer");
+    v3.push_back("q");
+    v3.push_back("wqeqwe");
+    v3.push_back("qweqweqwwee");
+    for(MyVector<string>::iterator it = v3.begin(); it != v3.end() + 1;++it)
     {
-        for (int i = 0; i < N / 2; ++i)
-        {
-            c.push_back(i + j);
-        }
-
-        for (int i : c)
-        {
-            res += i;
-        }
-
-        c.resize(N / 4);
-
-        for (int i : c)
-        {
-            res += i;
-        }
-
-        c.resize(N);
-
-        for (int i : c)
-        {
-            res += i;
-        }
-
-        for (int i = 0; i < N / 2; ++i)
-        {
-            c.pop_back();
-        }
-
-        for (int i : c)
-        {
-            res += i;
-        }
-
-        for (int i = 0; i < N / 2; ++i)
-        {
-            c.push_back(i + j);
-        }
-
-        for (int i : c)
-        {
-            res += i;
-        }
-
-        c.clear();
+        cout  << *it << endl;
     }
 
-    return res;
+    MyVector<string>::iterator iter = v3.end();
+    std::cout << *iter << std::endl;
+{
+    vector<int> v4;
+    v4.push_back(2);
+    MyVector<int> v;
+    MyVector<int> v2(212121);
+    v = v2;
+    cout << v.capacity() << endl;
 }
-
-template <class X, class Y>
-void doCheckEqual(const X& actual, const Y& expected, size_t line)
-{
-    if (actual != expected)
-    {
-        std::cout << "at line " << line << ": " << actual << " != " << expected << '\n';
-    }
-}
-
-#define checkEqual(x, y) do { doCheckEqual((x), (y), __LINE__); } while(0)
-#define checkTrue(cond) do { if (!(cond)) std::cout << "at line " << __LINE__ << ": " << #cond << '\n'; } while(0)
-
-static int Counter = 0;
-
-struct Counterable
-{
-    Counterable()
-    {
-        ++Counter;
-    }
-
-    Counterable(const Counterable&)
-    {
-        ++Counter;
-    }
-
-    Counterable& operator=(const Counterable&)
-    {
-        ++Counter;
-        return *this;
-    }
-
-    ~Counterable()
-    {
-        --Counter;
-    }
-};
-
-class Timer
-{
-public:
-    Timer()
-        : start_(std::chrono::high_resolution_clock::now())
-    {
-    }
-
-    ~Timer()
-    {
-        const auto finish = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
-    }
-
-private:
-    const std::chrono::high_resolution_clock::time_point start_;
-};
-
-int main()
-{
-    {
-        Vector<int> v;
-
-        checkTrue(v.empty());
-        checkEqual(v.size(), 0);
-
-        v.push_back(1);
-
-        checkTrue(!v.empty());
-        checkEqual(v.size(), 1);
-        checkEqual(v[0], 1);
-
-        v.pop_back();
-
-        checkTrue(v.empty());
-        checkEqual(v.size(), 0);
-
-        v.push_back(3);
-        v.push_back(2);
-        v.push_back(1);
-
-        checkTrue(!v.empty());
-        checkEqual(v.size(), 3);
-        checkEqual(v[0], 3);
-        checkEqual(v[1], 2);
-        checkEqual(v[2], 1);
-
-        auto r = v.rbegin();
-        checkTrue(r != v.rend());
-        checkEqual(*r, 1);
-        ++r;
-        checkTrue(r != v.rend());
-        checkEqual(*r, 2);
-        ++r;
-        checkTrue(r != v.rend());
-        checkEqual(*r, 3);
-        ++r;
-        checkTrue(r == v.rend());
-
-        auto f = v.begin();
-        checkTrue(f != v.end());
-        checkEqual(*f, 3);
-        ++f;
-        checkTrue(f != v.end());
-        checkEqual(*f, 2);
-        ++f;
-        checkTrue(f != v.end());
-        checkEqual(*f, 1);
-        ++f;
-        checkTrue(f == v.end());
-
-        v.reserve(10000);
-        checkEqual(v.size(), 3);
-        checkTrue(v.capacity() >= 10000);
-
-        const auto c = v.capacity();
-
-        v.resize(2);
-        checkEqual(v.size(), 2);
-        checkEqual(v.capacity(), c);
-        checkEqual(v[0], 3);
-        checkEqual(v[1], 2);
-
-        v.resize(3);
-        checkEqual(v.size(), 3);
-        checkEqual(v.capacity(), c);
-        checkEqual(v[0], 3);
-        checkEqual(v[1], 2);
-        checkEqual(v[2], 0);
-
-        v.resize(0);
-        checkEqual(v.size(), 0);
-        checkTrue(v.begin() == v.end());
-
-        v.resize(2);
-        checkEqual(v.size(), 2);
-        checkEqual(v[0], 0);
-        checkEqual(v[1], 0);
-    }
-
-    {
-        Vector<Counterable> v;
-        v.resize(100);
-
-        checkEqual(Counter, 100);
-
-        for (int i = 0; i < 100; ++i)
-        {
-            v.push_back(Counterable());
-        }
-
-        checkEqual(Counter, 200);
-
-        v.resize(150);
-
-        checkEqual(Counter, 150);
-
-        for (int i = 0; i < 100; ++i)
-        {
-            v.pop_back();
-        }
-
-        checkEqual(Counter, 50);
-
-        v.resize(25);
-
-        checkEqual(Counter, 25);
-
-        v.clear();
-
-        checkEqual(Counter, 0);
-
-        v.resize(25);
-
-        checkEqual(Counter, 25);
-    }
-
-    checkEqual(Counter, 0);
-
-    int res = 0;
-
-    {
-        std::vector<int> v;
-        res += benchmark(v);
-    }
-
-    {
-        std::cout << "Vector<int>: ";
-        Timer t;
-        Vector<int> v;
-        res += benchmark(v);
-    }
-
-    {
-        std::cout << "std::vector<int>: ";
-        Timer t;
-        std::vector<int> v;
-        res += benchmark(v);
-    }
-
-    {
-        std::cout << "std::deque<int>: ";
-        Timer t;
-        std::deque<int> v;
-        res += benchmark(v);
-    }
-
-    {
-        std::cout << "std::list<int>: ";
-        Timer t;
-        std::list<int> v;
-        res += benchmark(v);
-    }
-
-    return res;
 }
---
+++ 07/vector.h
@@ -0,0 +1,583 @@
+#include <exception>
+#include <type_traits>
+#include <iterator>
+
+#pragma once
+
+template<typename T>
+class MyAllocator
+{   
+public:
+
+    using value_type = T;
+    using pointer = value_type*;
+    using size_type = size_t;
+
+    MyAllocator();
+
+    pointer allocate(size_type);
+
+    void deallocate(pointer);
+
+    template<typename... Args>
+    void construct(pointer,Args&&...);
+
+    void destroy(pointer);
+
+    ~MyAllocator();
+};
+
+template<typename T>
+MyAllocator<T>::MyAllocator(){}
+
+template<typename T>
+MyAllocator<T>::~MyAllocator(){}
+
+template<typename T>
+template<typename... Args>
+void MyAllocator<T>::construct(pointer ptr,Args&&... args)
+{
+    ::new(static_cast<void*>(ptr)) value_type(std::forward<Args>(args)...);//constuctor from raw mem
+}
+
+template<typename T>
+void MyAllocator<T>::destroy(pointer ptr)
+{
+    //Complier automatically detects if it is primitive or not and to call destructor or not
+    static_cast<T*>(ptr)->~T();
+}
+
+template<typename T>
+typename MyAllocator<T>::pointer MyAllocator<T>::allocate(size_type elemNumber)
+{   
+    pointer ptr;
+    if ((ptr = static_cast<pointer>(::operator new(sizeof(value_type) * elemNumber))) == nullptr)//raw mem
+    {
+        throw std::bad_alloc();
+    }
+    else
+    {   
+        return ptr;
+    }
+}
+
+template<typename T>
+void MyAllocator<T>::deallocate(pointer ptr)
+{
+   ::operator delete(ptr);
+}
+
+
+template<typename Iter>
+class MyIterator : std::iterator<std::random_access_iterator_tag, Iter>//type of iterator and type of returned expression
+{
+public:
+
+    using value_type = Iter;
+    using pointer = value_type*;
+    using iterator = MyIterator<value_type>;
+    using index_type = int;
+
+    MyIterator(pointer,pointer);
+
+    MyIterator(const iterator&);
+    
+
+    MyIterator& operator=(const iterator&);
+
+    bool operator==(const iterator&) const;
+
+    bool operator!=(const iterator&) const;
+
+    bool operator>(const iterator&) const;
+
+    bool operator>=(const iterator&) const;
+
+    bool operator<(const iterator&) const;
+
+    bool operator<=(const iterator&) const;
+
+    value_type& operator[](index_type);
+
+    const value_type& operator[](index_type) const;
+
+    value_type& operator*();
+
+    const value_type operator*() const;
+
+    iterator operator+(index_type);
+
+    iterator& operator+=(index_type);
+
+    iterator operator-(index_type);
+
+    iterator& operator-=(index_type);
+
+    iterator& operator++();
+
+    void operator++(int);
+
+    iterator& operator--();
+
+    void operator--(int);
+
+private:
+    pointer begin_;
+    pointer current_;
+};
+
+
+template<typename Iter>
+MyIterator<Iter>::MyIterator(pointer begin, pointer from): begin_(begin),current_(from){}
+
+template<typename Iter>
+MyIterator<Iter>::MyIterator(const iterator& another): begin_(another.begin_),current_(another.current_){}
+
+template<typename Iter>
+MyIterator<Iter>& MyIterator<Iter>::operator=(const iterator& another)
+{
+    begin_ = another.begin_;
+    current_ = another.current_;
+    return *this;
+}
+
+template<typename Iter>
+bool MyIterator<Iter>::operator==(const iterator& another) const
+{
+    return current_ == another.current_;
+}
+
+template<typename Iter>
+bool MyIterator<Iter>::operator!=(const iterator& another) const
+{
+    return !(*this == another);
+}
+
+template<typename Iter>
+bool MyIterator<Iter>::operator>(const iterator& another) const
+{
+    return current_ > another.current_;
+}
+
+template<typename Iter>
+bool MyIterator<Iter>::operator>=(const iterator& another) const
+{
+    return current_ >= another.current_;
+}
+
+template<typename Iter>
+bool MyIterator<Iter>::operator<(const iterator& another) const
+{
+    return current_ < another.current_;
+}
+
+template<typename Iter>
+bool MyIterator<Iter>::operator<=(const iterator& another) const
+{
+    return current_ <= another.current_;
+}
+
+
+template<typename Iter>
+typename MyIterator<Iter>::value_type& MyIterator<Iter>::operator[](index_type ind)
+{
+    return *(begin_ + ind);
+}
+
+template<typename Iter>
+const typename MyIterator<Iter>::value_type& MyIterator<Iter>::operator[](index_type ind) const
+{
+    return *(begin_ + ind);
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::value_type& MyIterator<Iter>::operator*()
+{
+    return *current_;
+}
+
+template<typename Iter>
+const typename MyIterator<Iter>::value_type MyIterator<Iter>::operator*() const
+{
+    return *current_;
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::iterator MyIterator<Iter>::operator+(index_type ind)
+{
+    return iterator(begin_,current_ + ind);
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::iterator& MyIterator<Iter>::operator+=(index_type ind)
+{
+    current_ += ind;
+    return *(this);
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::iterator MyIterator<Iter>::operator-(index_type ind)
+{
+    return iterator(begin_,current_ - ind);
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::iterator& MyIterator<Iter>::operator-=(index_type ind)
+{
+    current_ -= ind;
+    return *(this);
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::iterator& MyIterator<Iter>::operator++()
+{
+    ++current_;
+    return *this;
+}
+
+template<typename Iter>
+void MyIterator<Iter>::operator++(int)
+{
+    current_++;
+}
+
+template<typename Iter>
+typename MyIterator<Iter>::iterator& MyIterator<Iter>::operator--()
+{
+    --current_;
+    return *this;
+}
+
+template<typename Iter>
+void MyIterator<Iter>::operator--(int)
+{
+    current_--;
+}
+
+
+
+template <typename T,typename Allocator = MyAllocator<T>>
+class MyVector
+{
+public:
+
+    using value_type = T;
+    using pointer = value_type*;
+    using vector = MyVector<T>;
+    using iterator = MyIterator<T>;
+    using allocator_type = MyAllocator<T>;
+    using size_type = size_t;
+
+explicit MyVector(size_type initialLength = 0);
+
+    MyVector(const vector&);
+
+    MyVector(const vector&,const allocator_type&);
+   
+    MyVector& operator=(const vector&);
+
+    void reserve(size_type);
+
+
+    void resize(size_type);
+
+    void resize(size_type,const value_type&);
+
+    void clear();
+
+    void push_back(const value_type&);
+
+    void push_back(value_type&&);
+
+    void pop_back();
+
+    bool empty();
+
+    iterator begin();
+
+    iterator end();
+
+    iterator rbegin();
+
+    iterator rend();
+
+    allocator_type get_allocator() const;
+
+    value_type& operator[](size_type);
+
+    const value_type operator[](size_type) const;
+
+    void shrink_to_fit();
+
+    size_type capacity() const;
+
+    size_type size() const ;
+
+    ~MyVector();
+
+private:
+    pointer first_;
+    pointer last_;
+    pointer end_;
+
+    Allocator allocator_;
+    
+   
+    void copy(pointer dest,pointer src,size_type size);
+
+
+    void fillwith_(pointer src,size_type from,const value_type& value);
+
+    void reallocate_(size_type newCapacity);
+
+    void deleteVector_();
+};
+
+
+
+template<typename T,typename Allocator>
+MyVector<T,Allocator>::MyVector(size_type initialLength)
+{
+    first_ = allocator_.allocate(initialLength);
+    last_ = first_;
+    
+    end_ = first_ +  initialLength;
+}
+
+template<typename T,typename Allocator>
+MyVector<T,Allocator>::MyVector(const vector& other)
+{
+    first_ = allocator_.allocate(other.capacity());
+    last_ = first_ + other.size();
+    end_ = first_ + other.capacity();
+
+    copy(first_,other.first_,other.size());
+}
+
+template<typename T,typename Allocator>
+MyVector<T,Allocator>::MyVector(const vector& other,const allocator_type& allocator)
+{
+    allocator_ = allocator;
+    first_ = allocator_.allocate(other.capacity());
+    last_ = first_ +  other.size();
+    end_ = first_ +  other.capacity();
+
+    copy(first_,other.first_,other.size());
+}
+
+template<typename T,typename Allocator>
+MyVector<T,Allocator>& MyVector<T,Allocator>::operator=(const vector& other)
+{
+    deleteVector_();
+    allocator_ = other.get_allocator(); 
+    first_ = allocator_.allocate(other.capacity());
+    last_ = first_ + other.size();
+    end_ = first_ + other.capacity();
+
+    copy(first_,other.first_,other.size());
+
+    return *this;
+}
+
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::reserve(size_type newCapacity)
+{
+    if (newCapacity > capacity())
+    {   
+        reallocate_(newCapacity);
+    }
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::resize(size_type newSize)
+{
+    if (size() < newSize)
+    {
+        size_type oldSize = size();
+        reallocate_(newSize);
+        last_ = first_ + newSize;
+        fillwith_(first_,oldSize,value_type());
+    }
+    else if (size() > newSize)
+    {
+        last_ = first_ + newSize;
+        reallocate_(newSize);
+    }
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::resize(size_type newSize,const value_type& value)
+{
+    if (size() < newSize)
+    {
+        size_type oldSize = size();
+        reallocate_(newSize);
+        last_ = first_ + newSize;
+        fillwith_(first_,oldSize,value);
+    }
+    else if (size() > newSize)
+    {
+        last_ = first_ +  newSize;
+        reallocate_(newSize);
+    }
+}
+
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::clear()
+{
+    for(size_type i = 0;i < size();++i)
+    {
+        allocator_.destroy(first_ + i);
+    }
+    last_ = first_;
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::push_back(const value_type& elem)
+{   
+    if (last_ == end_)
+    {
+        size_type capacity_ = capacity();
+        capacity_ = capacity_ * 2 + 1;
+        reallocate_(capacity_);
+    }
+    allocator_.construct(first_ + size(),elem);
+    last_++;
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::push_back(value_type&& elem)
+{   
+    if (last_ == end_)
+    {
+        size_type capacity_ = capacity();
+        capacity_ = capacity_ * 2 + 1;
+        reallocate_(capacity_);
+    }
+    allocator_.construct(first_ + size(),std::forward<value_type>(elem));
+    last_++;        
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::pop_back()
+{   
+    if (size() > 0) last_--;
+}
+
+template<typename T,typename Allocator>
+bool MyVector<T,Allocator>::empty()
+{
+    return (size() == 0);
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::iterator MyVector<T,Allocator>::begin()
+{
+    return iterator(first_ ,first_);
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::iterator MyVector<T,Allocator>::end()
+{
+    return iterator(last_ - 1,last_ - 1);
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::iterator MyVector<T,Allocator>::rbegin()
+{
+    return iterator(last_ - 1,last_ - 1);
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::iterator MyVector<T,Allocator>::rend()
+{
+    return iterator(first_,first_ );
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::allocator_type MyVector<T,Allocator>::get_allocator() const
+{
+    return allocator_;
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::value_type& MyVector<T,Allocator>::operator[](size_type ind)
+{
+    return *(first_ + ind);
+}
+
+template<typename T,typename Allocator>
+const typename MyVector<T,Allocator>::value_type MyVector<T,Allocator>::operator[](size_type ind)  const
+{
+    return *(first_ + ind);
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::shrink_to_fit()
+{
+    reallocate_(size());
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::size_type MyVector<T,Allocator>::capacity() const
+{
+    return (end_ - first_);
+}
+
+template<typename T,typename Allocator>
+typename MyVector<T,Allocator>::size_type MyVector<T,Allocator>::size() const
+{
+    return (last_ - first_);
+}
+
+template<typename T,typename Allocator>
+MyVector<T,Allocator>::~MyVector()
+{
+    deleteVector_();        
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::copy(pointer dest,pointer src,size_type size)
+{
+    for(size_type i = 0;i < size;++i)
+    {
+        allocator_.construct(dest + i,src[i]);
+    }
+}
+
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::fillwith_(pointer src,size_type from,const value_type& value)
+{
+    for(size_type i = from;i < size();++i)
+    {
+        allocator_.construct(src + i,value);
+    }
+}
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::reallocate_(size_type newCapacity)
+{
+    size_type oldSize = size();
+
+    value_type buffer[oldSize];
+    copy(buffer,first_,oldSize);
+    
+    deleteVector_();
+    first_ = allocator_.allocate(newCapacity);
+    last_ = first_ +  oldSize;
+    end_ = first_ +  newCapacity;
+
+    copy(first_,buffer,oldSize);
+}
+template<typename T,typename Allocator>
+void MyVector<T,Allocator>::deleteVector_()
+{   
+    for(size_type i = 0;i < size();++i)
+    {
+        allocator_.destroy(first_ + i);
+    }
+    allocator_.deallocate(first_);
+    last_ = nullptr;
+    end_ = nullptr;
+}
